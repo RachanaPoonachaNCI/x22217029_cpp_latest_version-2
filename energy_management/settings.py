f43c69d64beb96af9a202582e131262db83be6da
EC2 Default User <ec2-user@ip-172-31-16-41.eu-north-1.compute.internal>
2023-12-01 18:24:10 +0000

    Updated EC2 IP

--- energy_management/settings.py
+++ energy_management/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = "django-insecure-$s(@2rqs%8k+y-q-w%cv2sz%d+r=edxj&x!&7g@q@0!yd29_^z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['x22217029-rachana.eba-pe9sxm8x.sa-east-1.elasticbeanstalk.com','54.94.28.77','3.253.239.218','127.0.0.1:8000','172.31.26.141','172.31.35.132','x22217029cpp-env2.eba-hemqevvf.sa-east-1.elasticbeanstalk.com','16587ee00eb34e81aa0fe948fe97ab2b.vfs.cloud9.eu-north-1.amazonaws.com','51.20.249.35','16.16.246.142','x22217029cpp-env.eba-gwxdfpri.eu-north-1.elasticbeanstalk.com','x22217029cpplatest-env.eba-gwxdfpri.eu-north-1.elasticbeanstalk.com','x22217029cpp-env-4.eba-7mqmtqfh.eu-north-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['x22217029-rachana.eba-pe9sxm8x.sa-east-1.elasticbeanstalk.com','54.94.28.77','3.253.239.218','127.0.0.1:8000','172.31.26.141','172.31.35.132','51.20.222.174','x22217029cpp-env2.eba-hemqevvf.sa-east-1.elasticbeanstalk.com','16587ee00eb34e81aa0fe948fe97ab2b.vfs.cloud9.eu-north-1.amazonaws.com','51.20.249.35','16.16.246.142','x22217029cpp-env.eba-gwxdfpri.eu-north-1.elasticbeanstalk.com','x22217029cpplatest-env.eba-gwxdfpri.eu-north-1.elasticbeanstalk.com','x22217029cpp-env-4.eba-7mqmtqfh.eu-north-1.elasticbeanstalk.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://c7403b93921e47b194ab5e41acac01b6.vfs.cloud9.eu-west-1.amazonaws.com','https://3.249.208.143','https://3.252.138.102','https://3.253.155.8','https://3.250.149.11','https://3.253.239.218','https://x22217029-rachana.eba-pe9sxm8x.sa-east-1.elasticbeanstalk.com','http://127.0.0.1:8000/','http://172.31.26.141','http://172.31.35.132','http://51.20.249.35','http://16.16.246.142','http://x22217029cpplatest-env.eba-gwxdfpri.eu-north-1.elasticbeanstalk.com','http://x22217029cpp-env-4.eba-7mqmtqfh.eu-north-1.elasticbeanstalk.com/']
+CSRF_TRUSTED_ORIGINS = ['https://c7403b93921e47b194ab5e41acac01b6.vfs.cloud9.eu-west-1.amazonaws.com','https://3.249.208.143','https://3.252.138.102','http://51.20.222.174','https://3.253.155.8','https://3.250.149.11','https://3.253.239.218','https://x22217029-rachana.eba-pe9sxm8x.sa-east-1.elasticbeanstalk.com','http://127.0.0.1:8000/','http://172.31.26.141','http://172.31.35.132','http://51.20.249.35','http://16.16.246.142','http://x22217029cpplatest-env.eba-gwxdfpri.eu-north-1.elasticbeanstalk.com','http://x22217029cpp-env-4.eba-7mqmtqfh.eu-north-1.elasticbeanstalk.com/']
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -117,7 +117,7 @@ AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
 AWS_S3_REGION_NAME = AWS_S3_REGION_NAME
 AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME
 AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_REGION_NAME = 'Global'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
