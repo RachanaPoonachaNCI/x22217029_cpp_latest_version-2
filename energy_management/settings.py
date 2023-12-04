73c2c63d839b3a319800484dc7037a6ba145f663
EC2 Default User <ec2-user@ip-172-31-16-41.eu-north-1.compute.internal>
2023-12-04 09:46:28 +0000

    Updated settings

--- energy_management/settings.py
+++ energy_management/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
         }
     }
 
-#s3 bucket credentials retrieved by importing config file which has environment variables defined
+#s3 bucket credentials retrieved by importing config file which has environment variables defined here
 from aws_cloud_services.s3_credentials import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME
 AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
